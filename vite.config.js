6c390532533ac4854248f26e5619f7eece1cdb9f
Aarjav jain <aarjavjain@192.168.0.196>
2026-08-03 15:26:31 +0530

    vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        product: resolve(__dirname, 'product.html')
+      }
+    }
+  }
+});
