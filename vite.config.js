a4a7411108b9ca28e677bc2d9bcc26a51dd5794c
Aarjav jain <aarjavjain@192.168.0.147>
2026-09-03 21:19:30 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,12 @@ export default defineConfig({
     }
   },
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true
+      }
+    }
   }
 });
